2ae72a23449f142f818475afe7a96eca5ed5e272
felipe baloneker <felipebaloneker23@gmail.com>
2022-06-29 13:01:12 -0300

    feat/ docker app

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        }
-      }
-    }
+    host: '0.0.0.0',
+    port: 3000
   }
 });
